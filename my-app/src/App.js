e98c2e760e0d1097e1eff53eaea8d3989e0816a9
Nasser Eddin Nasser <nasser@Nassers-MacBook-Pro.local>
2024-06-04 15:30:47 +0200

    qwe

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App">
+    </div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <HelloWorld />
       </header>
     </div>
   );
